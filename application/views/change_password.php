287c6186dfcb6c8dc90c5bb1cfe9839c824781a6
Raag786 <developer4@indglobal-consulting.com>
2016-10-04 13:12:39 +0530

    chang pass fr ela

--- application/views/change_password.php
+++ application/views/change_password.php
@@ -39,8 +39,6 @@
 										</div>
 									</article>
 								</div>
-
-								
 							</form>
 						</div>
 					</div>
@@ -50,3 +48,135 @@
 	</div>
 </section>
 <!-- 	CUSTOMER REGISTRATION ENDS-->
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('.customer_update_profile').load(function(){
+			var user_id = <?php echo $u_id; ?>;
+			jQuery.ajax({
+				type 	: "POST",
+				url  	: "/gmt/View_profile/view_profile",
+				dataType: "json",
+				data	: {
+					user_id : user_id
+				},
+				success: function(res){
+					if(res.status_code == 200){
+						$('.cust_up_prof')[0].reset();
+			            $.each(res.data, function(key, val) {
+			            	// $.each(val, function(k, v){
+			                    $('<option value="'+val['vid']+'">'+val['vtype']+'</option>').appendTo('.vehicle');
+			                // });
+			            });
+					}else{
+						alert('No Response for user profile. Please Contact Admin.');
+						window.location ='<?php echo site_url('userdashboard/place_transporter_order'); ?>';
+						console.log('No Response. Please Contact Admin.');
+					}
+				},
+				error: function(){
+					console.log('Something went wrong.');
+				}
+			}); // ajax
+		}); // section load
+
+		// Ajax post for submiting registration form
+   	$(".cus-reg-sbmit").click(function(event) {
+   		var objCurrentSection = $('.tab-content div.active');       		
+   		
+   		var captcha 		= objCurrentSection.find('#captcha').val();
+        var captcha_word 	= $('input:hidden[name=hidden_captcha]').val();
+        
+        var first_name 		= objCurrentSection.find("#F_name").val();
+		var last_name 		= objCurrentSection.find("#L_name").val();
+        var user_mob 		= objCurrentSection.find("#M_number").val();
+        var user_email 		= objCurrentSection.find("#E_mail").val();
+        var user_pass 		= objCurrentSection.find("#password").val();
+        var c_pass 			= objCurrentSection.find("#c_pass").val();
+        var address1 		= objCurrentSection.find("#C_addrss").val();
+        var address2 		= objCurrentSection.find("#C_addrss_2").val();
+        // var country 		= $("#Country").val();
+        var state 			= objCurrentSection.find("#State").val();
+        var district 		= objCurrentSection.find("#District").val();
+        var city 			= objCurrentSection.find("#City").val();
+        var pin 			= objCurrentSection.find("#Pincode").val();
+        var pkg_id 			= objCurrentSection.find("#pkg_id").val();
+        var user_type 		= objCurrentSection.find("#user_type").val();
+
+        // This four field are in company customer form
+        var designation 	= objCurrentSection.find("#Designatn").val();
+        var firm_name	 	= objCurrentSection.find("#FirmName").val();
+        var company_type	= objCurrentSection.find("#C_type").val();
+        var company_pan		= objCurrentSection.find("#email_phn").val();
+        
+        if(first_name == '' || user_mob == '' || user_pass == '' || address1 == '' || 
+        	state == '' || district == '' || city == '' || pin == '' || captcha == '' ){
+        	$('#form_validation_msg').empty();
+		    $('<p><strong>All * marked fields must not be empty.</strong></p>').appendTo('#form_validation_msg');
+        }
+        else if(designation == '' || firm_name == '' || company_type == '' || company_pan == ''){
+        	$('#form_validation_msg').empty();
+		    $('<p><strong>All * marked fields must not be empty.</strong></p>').appendTo('#form_validation_msg');
+        }
+		else if(captcha !== captcha_word){
+        	$('#form_validation_msg').empty();
+		    $('<p><strong>Captcha code is wrong.</strong></p>').appendTo('#form_validation_msg');
+        }
+        else if(objCurrentSection.find("input[name=check1]").prop('checked') === false){
+		   	$('#form_validation_msg').empty();
+			$('<p><strong>Please Read and Accept our Terms of Service and Privacy Policy.</strong></p>').appendTo('#form_validation_msg');
+		}else{
+
+	        $.ajax({
+		        type: "POST",
+		        url: "/gmt/User/user_signup",
+		        cache: false,
+		        dataType: 'json',
+		        data: {
+		        	first_name 	: first_name,
+					last_name 	: last_name,
+					user_mob 	: user_mob,
+					user_email 	: user_email,
+					user_pass	: user_pass,
+					c_pass 		: c_pass,
+					address1 	: address1,
+					address2 	: address2,
+					state 		: state,
+					district 	: district,
+					city 		: city,
+					pin 		: pin,
+					pkg_id 		: pkg_id,
+					user_type 	: user_type,
+					designation : designation,
+					firm_name 	: firm_name,
+					company_type: company_type,
+					pan 		: company_pan
+		       	},
+		        success: function(res) {
+		            if (res.status_code == 200)
+		            {
+		              	$('#form_validation_msg').empty();
+			            $('<p><strong>Registered Successfully.</strong></p>').appendTo('#form_validation_msg');
+		              	$('.reg_form')[0].reset();
+			            /*$.each(res.data, function(key, val) {
+			            	$.each(val, function(k, v){
+			                    $('<li>'+v+'</li>').appendTo('#test');
+			                });
+			            });*/
+		            }else{
+			            $('#form_validation_msg').empty();
+			            $.each(res.data, function(key, val) {
+			            	$('<p><strong>'+val+'</strong></p>').appendTo('#form_validation_msg');
+			            });
+		            }
+	          	},
+		        error: function(){
+		        	console.log('Somthing went wrong');
+		        }
+	        });
+	    }
+    });
+
+
+		
+	}); // doc ready 
+</script>
